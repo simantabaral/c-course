57f64074e2d353bfc7cdf7ed6c4cb16ad795a9b6
Simanta Baral <33787055+simantabaral@users.noreply.github.com>
2018-01-18 11:02:32 +0545

    Add files via upload

---
+++ symbolipattern.c
@@ -0,0 +1,17 @@
+/*symbolic pattern*/
+#include<stdio.h>
+
+void main()
+{
+    int i;
+    int j;
+     printf("\n Symbolic Pattern\n\n");
+      for(i=1;i<=5;i++)
+      {
+          for(j=1;j<=i;j++)
+          {
+              printf("*");
+          }
+          printf("\n");
+      }
+}
