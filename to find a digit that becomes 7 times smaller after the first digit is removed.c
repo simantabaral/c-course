0f906db99ecb08858601f8757377d82f5bdd83b2
Simanta Baral <33787055+simantabaral@users.noreply.github.com>
2018-02-10 14:57:07 +0545

    Add files via upload

---
+++ to find a digit that becomes 7 times smaller after the first digit is removed.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int a,b;
+     b=a%10;
+    for(a=10;a<=99;a++)
+    {
+	
+    if((b*7)==a)
+
+           {
+		printf("xcc %d",a);	} 
+
+
+
+    }
+}
